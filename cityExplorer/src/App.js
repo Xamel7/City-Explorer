d059de7123cbcbaa3efc4eb491d52bcd77e956e7
Xamel7 <123972550+Xamel7@users.noreply.github.com>
2023-05-22 13:08:14 -0500

    Lab 06

--- cityExplorer/src/App.js
+++ cityExplorer/src/App.js
@@ -7,35 +7,42 @@ import axios from 'axios';
 import { useState } from 'react';
 
 function App() {
-  let typedText = ""
   const [displayCityInfo, setDisplayCityInfo] = useState("");
   const [inputCity, setInputCity] = useState("");
+  const [error, setError] = useState(null)
+  const mapData = `https://maps.locationiq.com/v3/staticmap?key=${process.env.
+    REACT_APP_LOCATION_API_KEY}&center=${displayCityInfo.lat},${displayCityInfo.lon}&zoom=13`
 
-  function radar(event){
+  function radar(event) {
     setInputCity(event.target.value)
   }
 
-  const button = async () => {
-    const apiKey = await axios.get(`https://us1.locationiq.com/v1/search?key=${process.env.
-    REACT_APP_LOCATION_API_KEY}&q=${inputCity}&format=json`)
-      .then(async function(response){
+  const button = () => {
+    const apiKey = axios.get(`https://us1.locationiq.com/v1/search?key=${process.env.
+      REACT_APP_LOCATION_API_KEY}&q=${inputCity}&format=json`)
+      .then(function (response){
         setDisplayCityInfo(response.data[0])
-      })
-      
-      return apiKey
+      }).catch(function (error) {
+          setError(error.message)
+        })
+
+    return apiKey
   }
-  
+
+  let mapElement = displayCityInfo ? <img src={mapData} alt="mapImage" /> : <></>
+
   return (
     <div className="App">
       <header className="App-header">
-        
+
 
         <Form.Control onChange={radar} type="text" placeholder="Explore the world!" />
-        <Button onClick={ button }>Explore!</Button>
+        <Button onClick={button}>Explore!</Button>
         {displayCityInfo.display_name}
         {displayCityInfo.lon}
         {displayCityInfo.lat}
-
+        {mapElement}
+        {error}
       </header>
     </div>
   );
